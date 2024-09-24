8b7b9736dbb27b0aea1f1adb4c92f0998c78896b
pwnaxe <biuro.norsk@gmail.com>
2024-09-24 15:12:00 +0200

    added navbar

--- frontend/src/app/components/navbar.js
+++ frontend/src/app/components/navbar.js
@@ -3,6 +3,8 @@
 
 export default function Navbar() {
     return (
+        <div className='bg-neutral-500 flex flex-row justify-center items-center py-4 text-white text-xl'>
         <h1>NAVBAR</h1>
+        </div>
     )
 }
